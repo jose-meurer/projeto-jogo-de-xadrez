7b2065b7cfcf3dc3e2ff20bd71027279c7d0ffa3
Jose Meurer <josewm18@gmail.com>
2021-06-16 11:01:23 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,9 +5,9 @@ import chess.ChessPiece;
 public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
-        for (int i = 0; i < pieces.length; i++){
+        for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
-            for (int x = 0; x < pieces.length; x++){
+            for (int x = 0; x < pieces.length; x++) {
                 printPiece(pieces[i][x]);
             }
             System.out.println();
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,9 +11,10 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
-    public ChessPiece[][] getpieces(){
+    public ChessPiece[][] getpieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0; i < board.getRows(); i++) {
             for (int x = 0; x < board.getColumns(); x++) {
@@ -19,5 +23,12 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+
     }
 }
